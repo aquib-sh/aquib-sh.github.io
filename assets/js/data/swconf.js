8281ec5d69b11e57a31379e4b283c943ab36513b
aquib-sh <aquib-sh@users.noreply.github.com>
2025-06-17 06:32:54 +0000

    deploy: e277ae22f6d5a958a75a5d28ef524a787d4bc104

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1750141140',resources: [
+    cacheName: 'chirpy-1750141969',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
